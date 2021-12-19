c3e146768e6a5a6748f9e5c69d2c0fc080621268
Doug Ryan <ryan_djr@yahoo.com>
2021-12-19 15:57:13 -0800

    Add project files.

---
+++ Lab02/main.cpp
@@ -0,0 +1,19 @@
+//**********************************************************************
+// File:				main.cpp
+// Author:			
+// Date:				
+// Class:				Intro to C++
+// Assignment:	Lab02 - Simple Math
+// Purpose:			Displays the sum and difference of two integers entered
+//							by the user at the keyboard
+// Hours:				
+//**********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
